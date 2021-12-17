c0d39ecb29f3f5b4e7e439c598306b83fd3790ba
Thiago <nagaoka.thiago@bol.com.br>
2021-12-17 16:48:30 -0300

    first commit

---
+++ src/main/java/com/desafio/estoque/EstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.estoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueApplication.class, args);
+	}
+
+}
